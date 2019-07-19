bce6f50e670beba7a8564fe61eb0bdce775d3c57
reganperkins <reganlperkins@gmail.com>
2019-07-19 11:28:27 -0700

    add server file

---
+++ express_server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const app = express();
+const PORT = 8080;
+
+const urlDatabase = {
+  'b2xVn2': 'http://www.lighthouselabs.ca',
+  '9sm5xK': 'http://www.google.com',
+};
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+app.get('/urls.json', (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
