f5c3233c44f5ef1aa8076f3d2830e64cd14c9326
reganperkins <reganlperkins@gmail.com>
2019-07-19 19:49:58 -0700

    add new post route, add body parser lib

--- express_server.js
+++ express_server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = 8080;
@@ -8,20 +9,35 @@ const urlDatabase = {
   '9sm5xK': 'http://www.google.com',
 };
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
-app.get('/urls.json', (req, res) => {
-  res.json(urlDatabase);
-});
+
 app.get('/urls', (req, res) => {
   const templateVars = {
     urls: urlDatabase,
+    // longURL: req.params.longURL,
   };
   res.render('urls-index', templateVars);
 });
+
+app.post('/urls', (req, res) => {
+  console.log(req.body)
+  res.send('okay');
+});
+
+app.get('/urls.json', (req, res) => {
+  res.json(urlDatabase);
+});
+
+// should be post
+app.get('/urls/new', (req, res) => {
+  res.render('new-url');
+});
+
 app.get('/urls/:shortURL', (req, res) => {
   const { shortURL } = req.params;
   const templateVars = {
